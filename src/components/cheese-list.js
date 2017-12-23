38cbab4cff708f99f386a6487137de0abb716ec8
Kayla Webb <kaylarwebb@gmail.com>
2017-12-23 15:33:39 -0500

    Renders hello world from cheese list component

---
+++ src/components/cheese-list.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class CheeseList extends React.Component {
+  constructor(props) {
+    super(props);
+    console.log(props)
+  }
+
+  render() {
+    return (
+      <div>Hello World!</div>
+    )
+  }
+
+}
--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CheeseList from './components/cheese-list'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<CheeseList />, document.getElementById('root'));
 registerServiceWorker();
